81bcd3a390ba54f6716a1f4ec1b79be39ee8ce5d
Kazuho Oku <kazuhooku@gmail.com>
2010-01-02 01:31:43 +0000

    Import of KAZUHO/Server-Starter-0.06 from CPAN.
    gitpan-cpan-distribution: Server-Starter
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         KAZUHO/Server-Starter-0.06.tar.gz
    gitpan-cpan-author:       KAZUHO
    gitpan-cpan-maturity:     released

--- inc/Test/TCP.pm
+++ inc/Test/TCP.pm
@@ -3,13 +3,16 @@ package Test::TCP;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '0.11';
+our $VERSION = '0.15';
 use base qw/Exporter/;
 use IO::Socket::INET;
 use Test::SharedFork;
 use Test::More ();
 use Config;
 use POSIX;
+
+# process does not die when received SIGTERM, on win32.
+my $TERMSIG = $^O eq 'MSWin32' ? 'KILL' : 'TERM';
 
 our @EXPORT = qw/ empty_port test_tcp wait_port /;
 
@@ -23,7 +26,7 @@ sub empty_port {
             LocalAddr => '127.0.0.1',
             LocalPort => $port,
             Proto     => 'tcp',
-            ReuseAddr => 1,
+            (($^O eq 'MSWin32') ? () : (ReuseAddr => 1)),
         );
         return $port if $sock;
     }
@@ -41,22 +44,39 @@ sub test_tcp {
         # parent.
         wait_port($port);
 
-        eval {
-            $args{client}->($port, $pid);
-        };
-        my $err = $@;
+        my $sig;
+        my $err;
+        {
+            local $SIG{INT}  = sub { $sig = "INT"; die "SIGINT received\n" };
+            local $SIG{PIPE} = sub { $sig = "PIPE"; die "SIGPIPE received\n" };
+            eval {
+                $args{client}->($port, $pid);
+            };
+            $err = $@;
 
-        kill TERM => $pid;
-        waitpid( $pid, 0 );
-        if (WIFSIGNALED($?)) {
-            my $signame = (split(' ', $Config{sig_name}))[WTERMSIG($?)];
-            if ($signame =~ /^(ABRT|PIPE)$/) {
-                Test::More::diag("your server received SIG$signame");
+            # cleanup
+            kill $TERMSIG => $pid;
+            while (1) {
+                my $kid = waitpid( $pid, 0 );
+                if ($^O ne 'MSWin32') { # i'm not in hell
+                    if (WIFSIGNALED($?)) {
+                        my $signame = (split(' ', $Config{sig_name}))[WTERMSIG($?)];
+                        if ($signame =~ /^(ABRT|PIPE)$/) {
+                            Test::More::diag("your server received SIG$signame");
+                        }
+                    }
+                }
+                if ($kid == 0 || $kid == -1) {
+                    last;
+                }
             }
         }
 
+        if ($sig) {
+            kill $sig, $$; # rethrow signal after cleanup
+        }
         if ($err) {
-            die $err; # rethrow after cleanup.
+            die $err; # rethrow exception after cleanup.
         }
     }
     elsif ( $pid == 0 ) {
@@ -102,4 +122,4 @@ __END__
 
 =encoding utf8
 
-#line 188
+#line 240
